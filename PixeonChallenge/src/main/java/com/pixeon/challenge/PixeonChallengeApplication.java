4fcab889ea2e7f2aeb3a1eac01706b8098c3e1b0
igorllampa <igorllampa@gmail.com>
2020-12-03 22:44:12 -0300

    Project initialization and configurations

---
+++ PixeonChallenge/src/main/java/com/pixeon/challenge/PixeonChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pixeon.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixeonChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixeonChallengeApplication.class, args);
+	}
+
+}
---
+++ PixeonChallenge/src/test/java/com/pixeon/challenge/PixeonChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeon.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixeonChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
