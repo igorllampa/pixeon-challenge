2c6868b4b3fd61b1fbaf05c1d32d81d7a139cd68
igorllampa <igorllampa@gmail.com>
2020-12-04 13:54:47 -0300

    Update model classes

--- PixeonChallenge/src/main/java/com/pixeon/challenge/domain/model/HealthcareInstitution.java
+++ PixeonChallenge/src/main/java/com/pixeon/challenge/domain/model/HealthcareInstitution.java
@@ -25,7 +25,60 @@ public class HealthcareInstitution {
 	
 	@Size(max = 14)
 	private String cnpj;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HealthcareInstitution other = (HealthcareInstitution) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 	//@OneToMany(mappedBy = "venda", cascade = CascadeType.ALL)		
 	//private List<Exam> exams =  new ArrayList<>();
+	
+	
 }
