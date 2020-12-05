9c9a6ce24f271350d1559a4887a86fa136206039
igorllampa <igorllampa@gmail.com>
2020-12-05 10:36:29 -0300

    Config for tests

--- PixeonChallenge/src/main/java/com/pixeon/challenge/api/controller/ExamController.java
+++ PixeonChallenge/src/main/java/com/pixeon/challenge/api/controller/ExamController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -53,6 +54,7 @@ public class ExamController {
 		return ResponseEntity.ok(exam);
 	}
 	
+	@DeleteMapping("/remove/{id}")
 	public ResponseEntity<Void> remove(@PathVariable Long id){
 		if(!examRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
